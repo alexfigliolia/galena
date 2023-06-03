9e82cd41464f6a5ab62063bc458f0cc6a2fd68b3
Alex Figliolia <alexfigliolia@gmail.com>
2023-06-03 15:28:14 -0400

    Remove process references

--- src/Galena/Guards.ts
+++ src/Galena/Guards.ts
@@ -18,18 +18,16 @@ export class Guards {
    * developers know that they are attempting to manipulate a
    * unit of state that has not yet been initialized
    */
-  protected warnForUndefinedStates = this.developmentGuard(
-    <T extends Record<string, State<any>>, K extends Extract<keyof T, string>>(
-      name: K,
-      state: T
-    ) => {
-      if (!process.env.IGNORE_LAZY_STATE_WARNINGS && !(name in state)) {
-        console.warn(
-          `A unit of state with the name "${name}" does not yet exist on this Galena instance. If this is expected, you can ignore this warning. To make these warnings go away, you can enable this process argument - "IGNORE_LAZY_STATE_WARNING=1"`
-        );
-      }
+  protected warnForUndefinedStates<T extends Record<string, State<any>>>(
+    name: string,
+    state: T
+  ) {
+    if (!(name in state)) {
+      console.warn(
+        `A unit of state with the name "${name}" does not yet exist on this Galena instance. If this is expected, you can ignore this warning`
+      );
     }
-  );
+  }
 
   /**
    * Guard Duplicate States
@@ -38,30 +36,14 @@ export class Guards {
    * more than one state with the same name on a single
    * `Galena` instance
    */
-  protected guardDuplicateStates = this.developmentGuard(
-    <T extends Record<string, State<any>>, K extends Extract<keyof T, string>>(
-      name: K,
-      state: T
-    ) => {
-      if (name in state) {
-        throw new Error(
-          `A unit of state with the name "${name}" already exists on this Galena instance. Please re-name this new unit of state to something unique`
-        );
-      }
+  protected guardDuplicateStates<T extends Record<string, State<any>>>(
+    name: string,
+    state: T
+  ) {
+    if (name in state) {
+      console.warn(
+        `A unit of state with the name "${name}" already exists on this Galena instance. Please re-name this new unit of state to something unique`
+      );
     }
-  );
-
-  /**
-   * Development Guard
-   *
-   * Wraps a provided function in a check for `process.env.NODE_ENV`
-   * equaling "development". Returns the callback
-   */
-  private developmentGuard<F extends (...args: any[]) => void>(guard: F) {
-    return (...args: Parameters<F>) => {
-      if (process.env.NODE_ENV === "development") {
-        guard(...args);
-      }
-    };
   }
 }
